51f8d9923c807f3e8458f222e5e0552864ef2bb6
Gero Keller <gero.f.keller@gmail.com>
2021-09-24 13:29:10 +0200

    Adds `BuildConfig` class and tests

---
+++ lib/ci_toolkit/build_config.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module CiToolkit
+  # Provides utility for a build
+  class BuildConfig
+    attr_reader :build_number, :build_url
+
+    def initialize(
+      options = { build_number: ENV["BITRISE_BUILD_NUMBER"], build_url: ENV["BITRISE_BUILD_URL"] },
+      for_pull_request = !ENV["BITRISE_PULL_REQUEST"].nil?,
+      for_cron_job = !ENV["BITRISE_SCHEDULED_BUILD"].nil?
+    )
+      @build_number = options.[](:build_number)
+      @build_url = options.[](:build_url)
+      @for_pull_request = for_pull_request
+      @for_cron_job = for_cron_job
+    end
+
+    def for_pull_request?
+      @for_pull_request
+    end
+
+    def for_cron_job?
+      @for_cron_job
+    end
+  end
+end

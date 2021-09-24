c82d047f4450c36f34c2a23f2d298ad7ad1300ce
Gero Keller <gero.f.keller@gmail.com>
2021-09-25 01:40:03 +0200

    Removes non library files

--- lib/ci_toolkit.rb
+++ lib/ci_toolkit.rb
@@ -3,8 +3,6 @@
 require_relative "ci_toolkit/version"
 
 require "ci_toolkit/app_config"
-require "ci_toolkit/app_store_config"
-require "ci_toolkit/app_store_connect_api_key"
 require "ci_toolkit/build"
 require "ci_toolkit/build_config"
 require "ci_toolkit/duplicate_files_finder"

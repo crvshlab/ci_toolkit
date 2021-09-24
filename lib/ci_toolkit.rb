d88740a468fafa35ba5bf20d3804d4580a607f6f
Gero Keller <gero.f.keller@gmail.com>
2021-09-24 13:17:34 +0200

    Adds `github_pr` and `git` to `CiToolkit` module

--- lib/ci_toolkit.rb
+++ lib/ci_toolkit.rb
@@ -2,6 +2,8 @@
 
 require_relative "ci_toolkit/version"
 require "ci_toolkit/github_access"
+require "ci_toolkit/github_pr"
+require "ci_toolkit/git"
 
 module CiToolkit
   class Error < StandardError; end

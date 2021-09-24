2e3efa99536415f144b5e7b6dfcee08a8b33660d
Gero Keller <gero.f.keller@gmail.com>
2021-09-24 10:59:45 +0200

    Use new default initializer values in tests

--- spec/github_access_spec.rb
+++ spec/github_access_spec.rb
@@ -2,15 +2,13 @@
 
 require "ci_toolkit"
 
-sut = CiToolkit::GithubAccess.new(
-  ENV["CRVSH_BOT_GITHUB_APP_ID"],
-  ENV["CRVSH_BOT_GITHUB_APP_PRIVATE_KEY"]
-)
+sut = CiToolkit::GithubAccess.new
 
 describe CiToolkit::GithubAccess do
   it "should provide a valid token" do
     token = sut.create_token
     expect(token).not_to be_empty
-    expect(token&.length).to be 40
+    # noinspection RubyNilAnalysis
+    expect(token.length).to be 40
   end
 end

ef0308a2ec09459fcd5092a42e8bdd2321d4e892
Gero Keller <gero.f.keller@gmail.com>
2021-09-28 00:40:51 +0200

    Adding some more debug output

--- transform_coverage_data.rb
+++ transform_coverage_data.rb
@@ -6,7 +6,9 @@ require "pathname"
 # The coverage data in resultset.json from SimpleCov contains absolute file paths
 # As that can confuse the sonarcloud sonar-scanner if it's running in a Docker for example
 # In the block below we are transforming the absolute file paths to relative paths
+puts "Transforming coverage/.resultset.json to sonar-scanner valid format in coverage/.resultset.sonar.json"
 json = JSON.parse(File.read("coverage/.resultset.json"))
+puts "Fixing file paths…"
 json["RSpec"]["coverage"].transform_keys! do |file|
   path = Pathname.new(file).relative_path_from(Pathname.new(File.expand_path("."))).to_s
   path.slice! ".."
@@ -14,7 +16,10 @@ json["RSpec"]["coverage"].transform_keys! do |file|
 
   "/github/workspace/#{path}"
 end
+puts "Fixing json line information…"
 json["RSpec"]["coverage"].transform_values! do |value|
   value["lines"]
 end
 File.write("coverage/.resultset.sonar.json", JSON.dump(json))
+puts "Wrote new coverage json to coverage/.resultset.sonar.json"
+puts `ls -lah coverage`

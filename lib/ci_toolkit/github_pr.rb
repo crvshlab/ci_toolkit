fdb448267e3aa02b74c026bd82f5a2d5205f0042
Gero Keller <gero.f.keller@gmail.com>
2021-09-29 20:29:47 +0200

    New access token is only generated once per `GithubPr` instance

--- lib/ci_toolkit/github_pr.rb
+++ lib/ci_toolkit/github_pr.rb
@@ -9,16 +9,17 @@ module CiToolkit
       env = CiToolkit::BitriseEnv.new,
       build_types = ENV["BUILD_TYPES"]&.split(/,/) || ["BluetoothDemo", "Acceptance PreProd", "Acceptance Prod",
                                                        "Latest Prod", "Latest PreProd", "Mock"],
-      client = Octokit::Client.new(access_token: CiToolkit::GithubAccess.new.create_token)
+      client = Octokit::Client.new
     )
       @pr_number = env.pull_request_number
       @repo_slug = env.repository_path
-      @client = client
+      @_client = client
       @build_types = build_types
+      @access = CiToolkit::GithubAccess.new
     end
 
     def title
-      @client.pull_request(@repo_slug, @pr_number).[](:title)
+      client.pull_request(@repo_slug, @pr_number).[](:title)
     end
 
     def number
@@ -26,16 +27,16 @@ module CiToolkit
     end
 
     def lines_of_code_changed
-      pr = @client.pull_request(@repo_slug, @pr_number)
+      pr = client.pull_request(@repo_slug, @pr_number)
       pr[:additions] + pr[:deletions]
     end
 
     def comments
-      @client.issue_comments(@repo_slug, @pr_number).map { |item| item[:body] }
+      client.issue_comments(@repo_slug, @pr_number).map { |item| item[:body] }
     end
 
     def comment(text)
-      @client.add_comment(@repo_slug, @pr_number, text[0...65_500]) # github comment character limit is 65536
+      client.add_comment(@repo_slug, @pr_number, text[0...65_500]) # github comment character limit is 65536
     end
 
     def delete_comment_containing_text(text)
@@ -44,24 +45,24 @@ module CiToolkit
     end
 
     def delete_comment(comment_id)
-      @client.delete_comment(@repo_slug, comment_id)
+      client.delete_comment(@repo_slug, comment_id)
     end
 
     def find_comment_containing_text(text)
       comment = nil
-      @client.issue_comments(@repo_slug, @pr_number).map do |item|
+      client.issue_comments(@repo_slug, @pr_number).map do |item|
         comment = item if item[:body]&.include? text
       end
       comment
     end
 
     def labels
-      @client.labels_for_issue(@repo_slug, @pr_number).map { |item| item[:name] }
+      client.labels_for_issue(@repo_slug, @pr_number).map { |item| item[:name] }
     end
 
     def create_status(state, context, target_url, description)
-      ref = @client.pull_request(@repo_slug, @pr_number).[](:head).[](:sha)
-      @client.create_status(
+      ref = client.pull_request(@repo_slug, @pr_number).[](:head).[](:sha)
+      client.create_status(
         @repo_slug,
         ref,
         state,
@@ -87,6 +88,15 @@ module CiToolkit
 
     def big?
       lines_of_code_changed > 500
+    end
+
+    private
+
+    def client
+      @_client = Octokit::Client.new if @_client.nil?
+      @_client.access_token = @access.create_token if @_client.access_token.nil?
+
+      @_client
     end
   end
 end

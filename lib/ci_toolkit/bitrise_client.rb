0c5ef9dfddbb99fcc64c23f866cfd1888bf83c4a
Gero Keller <gero.f.keller@gmail.com>
2021-10-04 14:47:55 +0200

    Default values for `BitriseClient` taken from ENV

--- lib/ci_toolkit/bitrise_client.rb
+++ lib/ci_toolkit/bitrise_client.rb
@@ -10,7 +10,7 @@ module CiToolkit
     API_VERSION = "v0.1"
     attr_reader :connection
 
-    def initialize(token, app_slug, faraday = Faraday.new)
+    def initialize(token = ENV["BITRISE_TOKEN"], app_slug = ENV["BITRISE_APP_SLUG"], faraday = nil)
       @token = token
       @app_slug = app_slug
       @connection = faraday
@@ -32,32 +32,32 @@ module CiToolkit
     end
 
     def create_pull_request_build(pull_request, branch, commit, workflow)
-      @connection.post("/apps/#{@app_slug}/builds", {
-                         hook_info: { type: "bitrise" },
-                         build_params: {
-                           branch: branch,
-                           branch_dest: "develop",
-                           pull_request_id: pull_request,
-                           workflow_id: workflow,
-                           commit_hash: commit
-                         }
-                       })
+      @connection&.post("/apps/#{@app_slug}/builds", {
+                          hook_info: { type: "bitrise" },
+                          build_params: {
+                            branch: branch,
+                            branch_dest: "develop",
+                            pull_request_id: pull_request,
+                            workflow_id: workflow,
+                            commit_hash: commit
+                          }
+                        })
     end
 
     def abort_pull_request_builds(pull_request, branch, commit)
       find_pull_request_builds(pull_request, branch, commit).each do |build|
-        @connection.post("/apps/#{@app_slug}/builds/#{build[:slug]}/abort", {
-                           abort_reason: "Aborting due to other build failed for pull request #{pull_request}"
-                         })
+        @connection&.post("/apps/#{@app_slug}/builds/#{build[:slug]}/abort", {
+                            abort_reason: "Aborting due to other build failed for pull request #{pull_request}"
+                          })
       end
     end
 
     def find_pull_request_builds(pull_request, branch, commit)
-      builds = @connection.get("/apps/#{@app_slug}/builds", {
-                                 branch: branch,
-                                 pull_request_id: pull_request.to_i,
-                                 status: 0 # status: 0 == not finished
-                               })
+      builds = @connection&.get("/apps/#{@app_slug}/builds", {
+                                  branch: branch,
+                                  pull_request_id: pull_request.to_i,
+                                  status: 0 # status: 0 == not finished
+                                })
       builds&.select! { |build| build[:commit_hash] == commit }
       builds || []
     end

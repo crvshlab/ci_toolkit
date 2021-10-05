f1553ae47199296436ed7f42b0614e7b641a5136
Gero Keller <gero.f.keller@gmail.com>
2021-10-06 00:43:02 +0200

    Added more tests

--- spec/ci_toolkit/bitrise_client_spec.rb
+++ spec/ci_toolkit/bitrise_client_spec.rb
@@ -67,4 +67,15 @@ describe CiToolkit::BitriseClient do
     sut.abort_pull_request_builds(123, "feature/my-pr", "some_commit_hash")
     expect(faraday).to have_received(:post).with("/v0.1/apps/a3rewrew4s5/builds/the_build_slug/abort", abort_pr_payload)
   end
+
+  it "filters builds by commit and the build number" do
+    sut = described_class.new({ build_number: 342, token: "some_token"}, double)
+    builds = sut.filter_builds_by_commit([{ "commit_hash" => "the_hash", "build_number" => 342 }], "the_hash")
+    expect(builds.length).to be 0
+  end
+
+  it "configures the connection if no faraday is provided" do
+    sut = described_class.new({ build_number: 342, token: "some_token"}, nil)
+    expect(sut.connection).not_to be nil
+  end
 end

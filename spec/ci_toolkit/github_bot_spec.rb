0b611a910dd7c60d322ab426ae902d59d04f45c5
Gero Keller <gero.f.keller@gmail.com>
2021-10-29 22:54:00 +0200

    Adds more tests for `GithubBot`

--- spec/ci_toolkit/github_bot_spec.rb
+++ spec/ci_toolkit/github_bot_spec.rb
@@ -4,12 +4,21 @@ require "ci_toolkit"
 require "rspec"
 
 describe CiToolkit::GithubBot do
+  creds = CiToolkit::GithubBot::Credentials.new(123)
+
   it "provides a valid token" do
-    creds = instance_spy("credentials")
     client = instance_spy("client")
     allow(client).to receive(:create_app_installation_access_token).and_return({ token: "23fdasfk43kjkk" })
 
     sut = described_class.new(creds, client)
     expect(sut.create_token).to eq "23fdasfk43kjkk"
+  end
+
+  it "finds the app installation" do
+    client = instance_spy("client")
+    allow(client).to receive(:find_app_installations).and_return([{ app_id: 123 }])
+
+    described_class.new(creds, client).create_token
+    expect(client).to have_received(:find_app_installations)
   end
 end

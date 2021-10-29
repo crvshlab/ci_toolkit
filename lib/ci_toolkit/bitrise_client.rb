5e94943952595e317b5c1f59c65bf0019647f03f
Gero Keller <gero.f.keller@gmail.com>
2021-10-29 11:00:50 +0200

    Version `1.3.14`
    Simplifies the creation logic of the connection used in the `BitriseClient`.

--- lib/ci_toolkit/bitrise_client.rb
+++ lib/ci_toolkit/bitrise_client.rb
@@ -19,51 +19,50 @@ module CiToolkit
       @build_number = options[:build_number].to_i
       @token = options[:token]
       @app_slug = options[:app_slug]
-      @connection = faraday
-      configure_connection
+      @connection = faraday || create_connection
     end
 
-    def configure_connection
-      return unless @connection.nil?
-
-      @connection = Faraday.new(
+    def create_connection
+      connection = Faraday.new(
         url: "https://api.bitrise.io",
         headers: { "Content-Type" => "application/json", "Authorization" => @token }
       )
-      @connection.use Faraday::Request::UrlEncoded
-      @connection.use Faraday::Request::Retry
-      @connection.use FaradayMiddleware::EncodeJson
-      @connection.use FaradayMiddleware::ParseJson
-      @connection.use FaradayMiddleware::FollowRedirects
+      connection.use Faraday::Request::UrlEncoded
+      connection.use Faraday::Request::Retry
+      connection.use FaradayMiddleware::EncodeJson
+      connection.use FaradayMiddleware::ParseJson
+      connection.use FaradayMiddleware::FollowRedirects
+
+      connection
     end
 
     def create_pull_request_build(pull_request, branch, commit, workflow)
-      @connection&.post("/#{API_VERSION}/apps/#{@app_slug}/builds", {
-                          hook_info: { type: "bitrise" },
-                          build_params: {
-                            branch: branch,
-                            branch_dest: "develop",
-                            pull_request_id: pull_request,
-                            workflow_id: workflow,
-                            commit_hash: commit
-                          }
-                        })
+      @connection.post("/#{API_VERSION}/apps/#{@app_slug}/builds", {
+                         hook_info: { type: "bitrise" },
+                         build_params: {
+                           branch: branch,
+                           branch_dest: "develop",
+                           pull_request_id: pull_request,
+                           workflow_id: workflow,
+                           commit_hash: commit
+                         }
+                       })
     end
 
     def abort_pull_request_builds(pull_request, branch, commit)
       find_pull_request_builds(pull_request, branch, commit).each do |build|
-        @connection&.post("/#{API_VERSION}/apps/#{@app_slug}/builds/#{build["slug"]}/abort", {
-                            abort_reason: "Aborting due to other build failed for pull request #{pull_request}"
-                          })
+        @connection.post("/#{API_VERSION}/apps/#{@app_slug}/builds/#{build["slug"]}/abort", {
+                           abort_reason: "Aborting due to other build failed for pull request #{pull_request}"
+                         })
       end
     end
 
     def find_pull_request_builds(pull_request, branch, commit)
-      response = @connection&.get("/#{API_VERSION}/apps/#{@app_slug}/builds", {
-                                    branch: branch,
-                                    pull_request_id: pull_request.to_i,
-                                    status: 0 # status: 0 == not finished
-                                  })
+      response = @connection.get("/#{API_VERSION}/apps/#{@app_slug}/builds", {
+                                   branch: branch,
+                                   pull_request_id: pull_request.to_i,
+                                   status: 0 # status: 0 == not finished
+                                 })
       builds = response.body["data"]
       filter_builds_by_commit(builds, commit)
     end

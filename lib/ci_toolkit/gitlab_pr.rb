baedd49081af0c50f3f8bdf78546b69873a01be0
Henry Addo <henry@addhen.org>
2022-06-19 20:03:07 +0200

    Implement Gitlab support for communicating with gitlab API

---
+++ lib/ci_toolkit/gitlab_pr.rb
@@ -0,0 +1,122 @@
+# frozen_string_literal: true
+
+require "gitlab"
+
+module CiToolkit
+  # Can be used to retrieve information about a PR on Github via the Github API
+  class GitlabPr
+    def initialize(
+      env = CiToolkit::BitriseEnv.new,
+      build_types = ENV["BUILD_TYPES"]&.split(/,/) || ["BluetoothDemo", "Acceptance PreProd", "Acceptance Prod",
+                                                       "Latest Prod", "Latest PreProd", "Mock", "Design System"],
+
+      client = Gitlab.client(endpoint: "https://gitlab.com/api/v4", private_token: "glpat-PyXxzywXzRXA94z8cU4g")
+    )
+      @pr_number = env.pull_request_number
+      @repo_slug = env.repository_path
+      @commit_sha = env.git_commit
+      @_client = client
+      @build_types = build_types
+      @bot = CiToolkit::GithubBot.new
+    end
+
+    def title
+      client.merge_request(@repo_slug, @pr_number).title || ""
+    end
+
+    def number
+      @pr_number
+    end
+
+    def lines_of_code_changed
+      pr = client.merge_request(@repo_slug, @pr_number)
+      pr.changes_count
+    end
+
+    def comments
+      client.merge_request_notes(@repo_slug, @pr_number).map { |item| item.body }
+    end
+
+    def comment(text)
+      client.create_merge_request_notes(@repo_slug, @pr_number, body: text[0...65_500]) # github comment character limit is 65536
+    end
+
+    def delete_comments_including_text(text)
+      comments = find_comments_including_text(text)
+      comments.each { |comment| delete_comment(comment.id) }
+    end
+
+    def delete_comment(comment_id)
+      client.delete_merge_request_note(@repo_slug, @pr_number, comment_id)
+    end
+
+    def find_comments_including_text(text)
+      comments = []
+      client.merge_request_notes(@repo_slug, @pr_number).map do |item|
+        comments << item if item.body&.include? text
+      end
+      comments
+    end
+
+    def labels
+      client.merge_request(@repo_slug, @pr_number).labels.map { |item| item }
+    end
+
+    def files
+      client.merge_request_changes(@repo_slug, @pr_number)
+    end
+
+    def create_status(state, context, target_url, description)
+      client.update_commit_status(
+        @repo_slug,
+        @commit_sha,
+        state,
+        { context: context, target_url: target_url, description: description }
+      )
+    end
+
+    def get_status(context)
+      client.commit_status(@repo_slug, @commit_sha).each do |status|
+        return status if status.name == context
+      end
+      nil
+    end
+
+    def build_types
+      types = []
+      @build_types.each do |type|
+        if comments.include?("#{type} build") || labels.include?("#{type} build") ||
+           comments.include?(type) || labels.include?(type)
+          types.push(type)
+        end
+      end
+      types
+    end
+
+    def infrastructure_work?
+      !(title =~ /\[infra\]/i).nil? || labels.include?("Infra")
+    end
+
+    def work_in_progress?
+      title.include?("[WIP]") || labels.include?("WIP")
+    end
+
+    def big?
+      lines_of_code_changed > 500
+    end
+
+    def realm_module_modified?
+      modified_files = files.select { |file| file.old_path&.start_with? "cache/" }
+      modified_files.length.positive?
+    end
+
+    private
+
+    def client
+      @_client = GitLab::Client.new if @_client.nil?
+      #@_client.access_token = @bot.create_token if @_client.access_token.nil?
+
+      @_client
+    end
+  end
+end
---
+++ spec/ci_toolkit/gitlab_pr_spec.rb
@@ -0,0 +1,172 @@
+# frozen_string_literal: true
+
+require "ci_toolkit"
+
+describe CiToolkit::GitlabPr do
+  env = CiToolkit::BitriseEnv.new({ pull_request_number: 100, repository_owner: "org", repository_slug: "repo" })
+
+  it "provides a title" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:pull_request).and_return({ title: "The PR title" })
+    expect(sut.title).to be "The PR title"
+  end
+
+  it "provides lines of code changed" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:pull_request).and_return({ additions: 10, deletions: 10 })
+    expect(sut.lines_of_code_changed).to be 20
+  end
+
+  it "has comments" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:issue_comments).and_return([{ body: "This is the comment text" }])
+    expect(sut.comments).to eq ["This is the comment text"]
+  end
+
+  it "adds a comment" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    sut.comment("my new text")
+    expect(client).to have_received(:add_comment).with("org/repo", 100, "my new text")
+  end
+
+  it "deletes a comment with text" do
+    client = instance_spy("client")
+    allow(client).to receive(:issue_comments).and_return([{ body: "example text", id: 12_345 }])
+    sut = described_class.new(env, [], client)
+    sut.delete_comments_including_text("example text")
+    expect(client).to have_received(:delete_comment).with("org/repo", 12_345)
+  end
+
+  it "checks for files modified in the realm module" do
+    client = instance_spy("client")
+    allow(client).to receive(:pull_request_files).and_return([{ filename: "cache/realm" }])
+    sut = described_class.new(env, [], client)
+    expect(sut).to be_realm_module_modified
+  end
+
+  it "correctly identifies that the realm module was not modified" do
+    client = instance_spy("client")
+    allow(client).to receive(:pull_request_files).and_return([{ filename: "a_different_file_name.jpg" }])
+    sut = described_class.new(env, [], client)
+    expect(sut).not_to be_realm_module_modified
+  end
+
+  it "does not error if the file doesn't have a filename" do
+    client = instance_spy("client")
+    allow(client).to receive(:pull_request_files).and_return([{}])
+    sut = described_class.new(env, [], client)
+    expect(sut).not_to be_realm_module_modified
+  end
+
+  it "does not delete a comment if it can't find the text" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    sut.delete_comments_including_text("example text")
+    expect(client).not_to have_received(:delete_comment).with("crvshlab/v-app-ios", 12_345)
+  end
+
+  it "provides labels" do
+    client = instance_spy("client")
+    allow(client).to receive(:labels_for_issue).and_return([{ name: "Label name" }])
+    sut = described_class.new(env, [], client)
+    expect(sut.labels).to eq ["Label name"]
+  end
+
+  it "creates a status check" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    sut.create_status("success", "Your check name",
+                      "https://target.url", "Your status description")
+    expect(client).to have_received(:create_status)
+  end
+
+  it "finds the build types from PR comments" do
+    client = instance_spy("client")
+    allow(client).to receive(:labels_for_issue).and_return([{ name: "WIP" }])
+    allow(client).to receive(:issue_comments).and_return([{ body: "build1 build" }])
+    sut = described_class.new(env, %w[build1 build2], client)
+    expect(sut.build_types).to eq %w[build1]
+  end
+
+  it "finds the build types from PR labels" do
+    client = instance_spy("client")
+    allow(client).to receive(:issue_comments).and_return([{ body: "Just a comment" }])
+    allow(client).to receive(:labels_for_issue).and_return([{ name: "build2 build" }])
+    sut = described_class.new(env, %w[build1 build2], client)
+    expect(sut.build_types).to eq ["build2"]
+  end
+
+  it "knows if PR is labeled as infrastructure work" do
+    client = instance_spy("client")
+    allow(client).to receive(:labels_for_issue).and_return([{ name: "Infra" }])
+    sut = described_class.new(env, [], client)
+    expect(sut.infrastructure_work?).to be true
+  end
+
+  it "knows if PR has a title showing infrastructure work" do
+    client = instance_spy("client")
+    allow(client).to receive(:pull_request).and_return({ title: "[INFRA]" })
+    sut = described_class.new(env, [], client)
+    expect(sut.infrastructure_work?).to be true
+  end
+
+  it "knows if PR is labeled as work in progress" do
+    client = instance_spy("client")
+    allow(client).to receive(:pull_request).and_return({ title: "The PR title" })
+    allow(client).to receive(:labels_for_issue).and_return([{ name: "WIP" }])
+    sut = described_class.new(env, [], client)
+    expect(sut.work_in_progress?).to be true
+  end
+
+  it "knows if PR has a title showing work in progress" do
+    client = instance_spy("client")
+    allow(client).to receive(:pull_request).and_return({ title: "[WIP]" })
+    sut = described_class.new(env, [], client)
+    expect(sut.work_in_progress?).to be true
+  end
+
+  it "knows if the PR is big" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:pull_request).and_return({ additions: 250, deletions: 251 })
+    expect(sut.big?).to be true
+  end
+
+  it "finds comment containing text" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:issue_comments).and_return([{ body: "This is a comment with some text" }])
+    expect(sut.find_comments_including_text("This is a comment")).to eq([{ body: "This is a comment with some text" }])
+  end
+
+  it "does not find comment a comment if there is no comment containing the search text" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:issue_comments).and_return([{ body: nil }])
+    expect(sut.find_comments_including_text("This is a comment")).to eq []
+  end
+
+  it "provides the correct pull request number" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    expect(sut.number).to eq 100
+  end
+
+  it "gets the status of a check" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:statuses).and_return([{ context: "check context" }])
+    expect(sut.get_status("check context")).not_to be_nil
+  end
+
+  it "provides a nil status if there is no check with the given context" do
+    client = instance_spy("client")
+    sut = described_class.new(env, [], client)
+    allow(client).to receive(:statuses).and_return([{ context: "check context" }])
+    expect(sut.get_status("a different context")).to be_nil
+  end
+end

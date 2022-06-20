f85331f64db008426972ebebb31741d91f3745bc
Henry Addo <henry@addhen.org>
2022-06-20 14:43:20 +0200

    Fix code style issues

--- lib/ci_toolkit/dvcs_pr_factory.rb
+++ lib/ci_toolkit/dvcs_pr_factory.rb
@@ -4,15 +4,15 @@ module CiToolkit
 
   class DvcsPrFactory
     SERVICES = {
-      'gitlab' => CiToolkit::GitlabPr,
-      'github' => CiToolkit::GithubPr
+      "gitlab" => CiToolkit::GitlabPr,
+      "github" => CiToolkit::GithubPr
     }
 
     private_constant :SERVICES
 
-    def self.create(bitriseEnv = CiToolkit::BitriseEnv.new)
-      service = ENV['DVCS_SERVICE']
-      (SERVICES[service.to_s.downcase] || CiToolkit::DvcsPr).new bitriseEnv
+    def self.create(bitrise_env = CiToolkit::BitriseEnv.new)
+      service = ENV["DVCS_SERVICE"]
+      (SERVICES[service.to_s.downcase] || CiToolkit::DvcsPr).new bitrise_env
     end
   end
 end
